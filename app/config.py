89141c5c9362260c213d1a54a98e4586ab474551
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-14 15:55:05 +0300

    Add url to get news articles

---
+++ app/config.py
@@ -0,0 +1,23 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_ARTICLE_API_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    # SOURCE_ARTICLE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey=80ec86b4b19148579a64f273b9f5a047'
+    
+class ProdConfig(Config):
+
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
