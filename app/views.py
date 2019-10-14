1000ccdbab62943b048d160c3ba53b2c329391e7
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-14 12:26:35 +0300

    displays list of news sources to the user

---
+++ app/views.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from app import app
+from .request import get_news
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+     # Getting popular movie
+    popular_news = get_news()
+    print(popular_news)
+    title = 'Welcome to the Best News Site in the region'
+    return render_template('index.html', title = title,name_news = popular_news)
